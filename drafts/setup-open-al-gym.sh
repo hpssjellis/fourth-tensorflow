1f168d3630e83d9e527306fca5023660a864ccef
Jeremy Ellis <keyfreemusic@gmail.com>
2016-05-10 22:23:22 -0700

    Create setup-open-al-gym.sh

---
+++ drafts/setup-open-al-gym.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash setup-open-al-gym.sh
+
+
+
+
+sudo apt-get update
+cd /home/ubuntu/workspace
+
+git clone https://github.com/openai/gym
+cd gym
+pip install gym # minimal install may try the next line instead
+
+#pip install gym[all]
+
+echo "See the docs at https://gym.openai.com/docs"
