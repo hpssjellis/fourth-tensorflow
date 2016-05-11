2227f6629e48c4d58d23a83098a17b722459071e
Jeremy Ellis <keyfreemusic@gmail.com>
2016-05-10 23:56:19 -0700

    Update z03-custom-model.py

--- skflow-examples/z03-custom-model.py
+++ skflow-examples/z03-custom-model.py
@@ -1,13 +1,16 @@
+#http://terrytangyuan.github.io/2016/03/14/scikit-flow-intro/
+
+
 from sklearn import datasets, metrics
 
 iris = datasets.load_iris()
 
 def my_model(X, y):
     """This is DNN with 10, 20, 10 hidden layers, and dropout of 0.5 probability."""
-    layers = learn.ops.dnn(X, [10, 20, 10], dropout=0.5)
-    return learn.models.logistic_regression(layers, y)
+    layers = skflow.ops.dnn(X, [10, 20, 10], keep_prob=0.5)
+    return skflow.models.logistic_regression(layers, y)
 
-classifier = learn.TensorFlowEstimator(model_fn=my_model, n_classes=3)
+classifier = skflow.TensorFlowEstimator(model_fn=my_model, n_classes=3)
 classifier.fit(iris.data, iris.target)
 score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
 print("Accuracy: %f" % score)
