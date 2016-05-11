e66fe9866e77d7b191692d530596bb43c9dd055b
Jeremy Ellis <keyfreemusic@gmail.com>
2016-05-10 23:37:33 -0700

    Create custom-model.py

---
+++ skflow-examples/custom-model.py
@@ -0,0 +1,13 @@
+from sklearn import datasets, metrics
+
+iris = datasets.load_iris()
+
+def my_model(X, y):
+    """This is DNN with 10, 20, 10 hidden layers, and dropout of 0.5 probability."""
+    layers = learn.ops.dnn(X, [10, 20, 10], dropout=0.5)
+    return learn.models.logistic_regression(layers, y)
+
+classifier = learn.TensorFlowEstimator(model_fn=my_model, n_classes=3)
+classifier.fit(iris.data, iris.target)
+score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
+print("Accuracy: %f" % score)
